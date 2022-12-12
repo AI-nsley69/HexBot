295b4fbfbdd55f2c141962a996ef3ee2ac53f0ab
20bear <20bear@stockholmscience.se>
2022-12-12 09:08:37 +0100

    Working example

--- index.js
+++ index.js
@@ -1,2 +1,64 @@
+// Secrets
+const { reddit, discord } = require("./secrets.json");
+
+// Axios for get requests
+const axios = require("axios");
+
+// Reddit api setup
+const { SubmissionStream } = require("snoostorm");
 const Snoowrap = require("snoowrap");
-const Snoostorm = require("snoostorm");
+const rclient = new Snoowrap(reddit);
+
+// Discord api setup
+const { Client, Intents, MessageEmbed } = require("discord.js");
+const intents = new Intents();
+intents.add(Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES);
+const dclient = new Client({
+  intents: intents,
+});
+
+dclient.login(discord.token);
+// Log to the console if bot was enabled
+dclient.on("ready", async () => {
+  console.log(`${dclient.user.tag} is online!`);
+  dclient.user.setActivity("Dislyte", {
+    type: "PLAYING",
+  });
+});
+
+const submissionEvent = new SubmissionStream(rclient, {
+  subreddit: "Dislyte",
+  limit: 10,
+  pollTime: 2000,
+});
+
+submissionEvent.on("item", async (post) => {
+  const flair = "Info";
+  if (post.link_flair_text != flair) return;
+  if (post.title.endsWith("?")) return;
+
+  const pastelPurple = 0xb1a2ca;
+  const embed = new MessageEmbed()
+    .setTitle(post.title)
+    .setURL(post.url)
+    .setColor(pastelPurple)
+    .setTimestamp();
+
+  if (post.is_video) embed.setImage(post.media.oembed.thumbnail_url);
+  if (isImage(url)) embed.setImage(url);
+  if (post.is_self) embed.setDescription(post.selftext);
+  if (post.author_name) {
+    embed.setAuthor({
+      name: post.author_name,
+    });
+  }
+
+  dclient.channels
+    .fetch(discord.updateChannel)
+    .then((ch) => ch.send({ embeds: [embed] }));
+});
+
+async function isImage(url) {
+  const res = await axios.get(url).then(res.headers);
+  return res.headers["content-type"].startsWith("image");
+}
